3bb96875a2d019fd30ecdf4c07c8198a6a1c377a
threedisk <threedisk@gmail.com>
2020-12-29 10:55:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#Data Types
+garis="============================="
+#String
+print ("String :")
+print ("Hello"[4])
+print ("123"+"456")
+print (garis)
+
+#Integer
+print ("Integer : ")
+print (123+456)
+print (garis)
+
+
+#Float
+print ("Float Number :")
+print (3.14)
+print (garis)
+
+#Boolean
+
+True
+False
+
+#Conversion data type
+name = input ("What is your name? ")
+char = len(name)
+#print (type(char))
+new_char = str(char)
+#print (type(new_char))
+print ("Your name has "+new_char+" characters ")
